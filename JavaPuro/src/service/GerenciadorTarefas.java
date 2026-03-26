a2b2ce7149aab5feba8dcc9ec9e66b442bfa27e2
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-03-26 17:39:36 -0300

    Update GerenciadorTarefas.java

--- JavaPuro/src/service/GerenciadorTarefas.java
+++ JavaPuro/src/service/GerenciadorTarefas.java
@@ -3,6 +3,7 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.StatusTarefa;
 import model.Tarefa;
 
 public class GerenciadorTarefas {
@@ -16,13 +17,41 @@ public class GerenciadorTarefas {
     }
 
     public void listarTarefas() {
+        if (tarefas.size() == 0) {
+            System.out.println("Sem tarefas no momento!");
+            return;
+        }
+
         for (Tarefa tarefa : tarefas) {
+            System.out.println("---------------------");
             System.out.println("ID: " + tarefa.getId());
             System.out.println("Titulo: " + tarefa.getTitulo());
             System.out.println("Status: " + tarefa.getStatus());
             System.out.println("Data: " + tarefa.getDataCriacao());
             System.out.println("---------------------");
-            
         }
+    }
+
+    public void atualizarStatus(Long id, StatusTarefa novoStatus) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                tarefa.setStatus(novoStatus);
+                System.out.println("Status atualizado!");
+                return;
+            }
+        }
+
+        System.out.println("Tarefa não encontrada");
+    }
+
+    public void removerTarefa(Long id) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId().equals(id)) {
+                tarefas.remove(tarefa);
+                System.out.println("Tarefa removida!");
+            }
+        }
+
+        System.out.println("Tarefa não encontrada");
     }
 }
