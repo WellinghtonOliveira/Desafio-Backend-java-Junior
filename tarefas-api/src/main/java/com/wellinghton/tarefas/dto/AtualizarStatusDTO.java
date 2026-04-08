1d56dbca0961d4a11ad57eb7ea76740025fb6529
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-04-07 23:06:05 -0300

    Squashed commit of the following:
    commit f6d0cee6523c5cd25a1eaa095d1f57ec1180865e
    Author: Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
    Date:   Tue Apr 7 22:55:44 2026 -0300
    
        Atualização
    
        BRANCH feita completamente usando Spring Boot

---
+++ tarefas-api/src/main/java/com/wellinghton/tarefas/dto/AtualizarStatusDTO.java
@@ -0,0 +1,15 @@
+package com.wellinghton.tarefas.dto;
+
+public class AtualizarStatusDTO {
+
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
